70af505ab5808de5861b69ffdd2e50ab5afca27f
Howard Huang <hao@hwrd.me>
2024-02-21 20:18:16 -0600

    Tool cleanup
    Don't require a check on `just build`, require it on `deploy` instead
    Fix lefthook quiet sections not being respected
    Fix Github Actions not installing Just properly
    Install go tools instead of relying on `go run <url>` since that is slow

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,7 +7,7 @@ mise install golang@latest
 mise use golang@latest
 mise exec golang@latest -- go mod init '{{ cookiecutter.module_path }}'
 make bootstrap
-mise exec just@latest -- just lint
-mise exec just@latest -- just test
+mise exec just@latest go@latest -- just lint
+mise exec just@latest go@latest -- just test
 git init && git add . && git commit -m "Initial commit, generated with cookiecutter"
 lefthook install
